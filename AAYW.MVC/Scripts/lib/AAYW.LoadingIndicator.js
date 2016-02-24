843cb0914bafa133c63add52b19753ee6bf2d1c6
AlexSolari <alexsanify@gmail.com>
2016-02-24 17:00:38 +0200

    Implemented animation for spinner

---
+++ AAYW.MVC/Scripts/lib/AAYW.LoadingIndicator.js
@@ -0,0 +1,16 @@
+﻿$(window).load(function () {
+    window.AAYW = window.AAYW || {};
+
+    AAYW.UI = AAYW.UI || {};
+
+    AAYW.UI.LoadingIndicator = AAYW.UI.LoadingIndicator || (function (window) {
+		return {
+			Show: function () {
+			    
+			},
+			Close: function () {
+				
+			},
+		};
+	})(this);
+});
--- AAYW.MVC/Scripts/lib/AAYW.Popup.js
+++ AAYW.MVC/Scripts/lib/AAYW.Popup.js
@@ -1,16 +1,35 @@
 ﻿$(window).load(function () {
-	window.AAYW = window.AAYW || {};
+    window.AAYW = window.AAYW || {};
 
-	AAYW.Popup = AAYW.Popup || (function (window) {
+    AAYW.UI = AAYW.UI || {};
+
+	AAYW.UI.Popup = AAYW.UI.Popup || (function (window) {
 		return {
 			Show: function (content) {
-				var modalEl = document.createElement('div');
+			    var closeRow = document.createElement('div');
 
+			    var closeButton = document.createElement('span');
+			    closeButton.className = 'popup-close-button';
+			    closeButton.innerHTML = "X";
+			    closeButton.style.marginLeft = '50%';
+			    closeButton.onclick = function () {
+			        mui.overlay('off');
+			    };
+
+			    var modalEl = document.createElement('div');
+			    modalEl.className = 'popup-containter';
 				modalEl.style.width = '100%';
 				modalEl.style.height = 'auto';
 				modalEl.style.margin = '100px auto';
 				modalEl.style.backgroundColor = '#fff';
-				modalEl.innerHTML = content;
+
+				var popupContent = document.createElement('div');
+				popupContent.innerHTML = content;
+				popupContent.className = "popup";
+
+				closeRow.appendChild(closeButton);
+				modalEl.appendChild(closeRow);
+				modalEl.appendChild(popupContent);
 
 				mui.overlay('on', modalEl);
 			},
