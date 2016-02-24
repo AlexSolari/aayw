c4f98d40821bf333c8ad16a6e617c24e1e66f2ee
AlexSolari <alexsanify@gmail.com>
2016-02-24 11:39:56 +0200

    Deleted trash

---
+++ AAYW.Core/Web/ControllerFactory/BaseControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using AAYW.Core.Dependecies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace AAYW.Core.Web.ControllerFactory
+{
+    public class BaseControllerFactory : DefaultControllerFactory
+    {
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            var controller = Resolver.GetController(controllerName);
+
+            return controller;
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            var disposable = controller as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+
+    }
+}
