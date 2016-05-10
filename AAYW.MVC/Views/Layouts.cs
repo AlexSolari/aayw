2cf78f301adff6e36d5614ff83fade9a762c4b51
AlexSolari <alexsanify@gmail.com>
2016-05-10 13:10:08 +0300

    Moved Layouts to constants

---
+++ AAYW.MVC/Views/Layouts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAYW.MVC.Views
+{
+    public class Layouts
+    {
+        public const string Admin = "~/Views/Admin/Layout.cshtml";
+        public const string Default = "~/Views/Shared/_Layout.cshtml";
+    }
+}
