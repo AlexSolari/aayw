127d20bf15fa393c631572910327ac964cc6364f
AlexSolari <alexsanify@gmail.com>
2016-11-28 16:27:00 +0200

    New types of requests

--- sORM/Core/SimpleORM.cs
+++ sORM/Core/SimpleORM.cs
@@ -60,6 +60,33 @@ namespace sORM.Core
             }
 
             Requests.Initialize();
+
+            var prepareRequest = new SqlRequest("IF EXISTS(SELECT* FROM sys.objects WHERE object_id = OBJECT_ID(N'DatabaseHealthCheck') AND type in (N'P', N'PC')) " +
+                    "DROP PROCEDURE DatabaseHealthCheck;");
+            var healthCheckRequest = new CreateProcedureRequest(@"
+            SELECT DISTINCT
+            OBJECT_NAME(s.[object_id]) AS TableName,
+            c.name AS ColumnName,
+            s.name AS StatName,
+            s.auto_created,
+            s.user_created,
+            s.no_recompute,
+            s.[object_id],
+            s.stats_id,
+            sc.stats_column_id,
+            sc.column_id,
+            STATS_DATE(s.[object_id], s.stats_id) AS LastUpdated
+            FROM sys.stats s JOIN sys.stats_columns sc 
+                          ON sc.[object_id] = s.[object_id] AND sc.stats_id = s.stats_id
+            JOIN sys.columns c ON c.[object_id] = sc.[object_id] AND c.column_id = sc.column_id
+            JOIN sys.partitions par ON par.[object_id] = s.[object_id]
+            JOIN sys.objects obj ON par.[object_id] = obj.[object_id]
+            WHERE OBJECTPROPERTY(s.OBJECT_ID,'IsUserTable') = 1
+            AND (s.auto_created = 1 OR s.user_created = 1);
+            ", "DatabaseHealthCheck");
+
+            Requests.Execute(prepareRequest);
+            Requests.Execute(healthCheckRequest);
         }
 
         public void CreateOrUpdate<T>(T obj)
